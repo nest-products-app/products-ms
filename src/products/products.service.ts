580eb7dd12a62557999290798ae4a53b77d015ae
David-Gcs <82048609+David-Gcs@users.noreply.github.com>
2025-02-02 00:02:17 -0400

    New API created

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -77,4 +77,25 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       },
     });
   }
+
+  async validateProducts(ids: number[]) {
+    ids = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
+    });
+
+    if (products.length !== ids.length) {
+      throw new RpcException({
+        message: 'Some products does not exist',
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+    return products;
+  }
 }
